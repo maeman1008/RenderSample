ef463bd53c290a52a46bb3e7712a2f31d8e19390
Ryoto Maeda <ryoto@ryotonoimac-puro.local>
2018-07-20 17:15:23 +0900

    Initial Commit

---
+++ RenderSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RenderSample
+//
+//  Created by Ryoto Maeda on 2018/07/20.
+//  Copyright © 2018年 thecoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
